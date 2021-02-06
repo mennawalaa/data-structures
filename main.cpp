be2aaff6262b29f1efa20cc7a6407a18b5b1d63d
mennahup <alaamahmoudabbais@gmail.com>
2021-02-07 01:14:06 +0200

    IMPLEMENTATION OF DATA_STRUCTURES USING Subject < 50 chars
    Body in detail

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 //#pragma once
-#include<iostream>
-#include"stacklist.cpp"
-void print_single(stack_input x){
-   
-    cout<<"value of user func"<<x<<endl;
+#include <iostream>
+#include "stacklist.cpp"
+void print_single(stack_input x)
+{
+
+    cout << "value of user func" << x << endl;
 }
-int main(){
-    cout<<"hello world"<<endl;
+int main()
+{
+    cout << "hello world" << endl;
     Stack s;
     stack_input x;
     bool y;
     bool r;
     stack_init(&s);
-    y=is_full(&s);
-    if(!y){
-        cout<<"the stack is full"<<endl;
+    y = is_full(&s);
+    if (!y)
+    {
+        cout << "the stack is full" << endl;
         return 0;
     }
-    for(int i=0;i<stack_size;++i){
-       cout<<"i"<<i<<endl;
-        push(i,&s);
+    for (int i = 0; i < stack_size; ++i)
+    {
+        cout << "i" << i << endl;
+        push(i, &s);
     }
-  /*  clear_stack(&s);
+    /*  clear_stack(&s);
     r=empty(&s);
     if(!r){
         cout<<"stack is empty"<<endl;
@@ -37,9 +41,9 @@ int main(){
      cout<<"stack values"<<x<<endl;
     }
     */
- reverse_print(&s,&print_single);
+    reverse_print(&s, &print_single);
 
-/*push(2,&s);
+    /*push(2,&s);
 stack_input x1;
 pop(&x1,&s);
 cout<<"x"<<x1<<endl;
@@ -47,5 +51,4 @@ stack_input y1;
 pop(&y1,&s);
 cout<<"y"<<y1<<endl;
 */
-
 }
--- queue.cpp
+++ queue.cpp
@@ -1,45 +1,49 @@
-#include<iostream>
-#include"queulist.cpp"
+#include <iostream>
+#include "queulist.cpp"
 using namespace std;
-void printelement(que_type w){
-    cout<<"output element"<<w<<endl;
+void printelement(que_type w)
+{
+    cout << "output element" << w << endl;
 }
-int main(){
+int main()
+{
     queue q;
-    int x,s;
+    int x, s;
     bool y;
-   
- // s= q.quesize();
- // cout<<"inital size"<<s<<endl;
-   y = q. append(10);
-    for(int i=0;i<5;++i){
-       bool v= q. append(i+10); 
-  }
 
-     y=q.full();
-     if(!y){
-         cout<<"full"<<endl;
-     }
+    // s= q.quesize();
+    // cout<<"inital size"<<s<<endl;
+    y = q.append(10);
+    for (int i = 0; i < 5; ++i)
+    {
+        bool v = q.append(i + 10);
+    }
+
+    y = q.full();
+    if (!y)
+    {
+        cout << "full" << endl;
+    }
     //  bool z=q. retrieve(&x);
-     //  cout<<"appended value main"<<x<<endl;
-  //    q.append(300);
- //    q.que_clear();
-  s= q.quesize();
-  cout<<"inital size"<<s<<endl;
-/* for(int i=0;i<s;++i){
+    //  cout<<"appended value main"<<x<<endl;
+    //    q.append(300);
+    //    q.que_clear();
+    s = q.quesize();
+    cout << "inital size" << s << endl;
+    /* for(int i=0;i<s;++i){
       bool u= q. retrieve(&x);
           cout<<"appended value main"<<x<<endl;
    }*/
-   
-  //  q. retrieve(&x);
-  // cout<<"appended value main"<<x<<endl;
- /*  bool z;
+
+    //  q. retrieve(&x);
+    // cout<<"appended value main"<<x<<endl;
+    /*  bool z;
    z=q.empty();
    if(!z){
        cout<<"empty"<<endl;
    }*/
-//  q. tryyyy();
-q. print_reverse(& printelement);
+    //  q. tryyyy();
+    q.print_reverse(&printelement);
 
     return 0;
 }
